09dc1e5e6eb2495e1b2dc7da0d9b3827ad77eb5a
kalmaro <aseramiazhka@exadel.com>
2014-12-24 02:05:48 +0200

    implement team

--- Team/Team.js
+++ Team/Team.js
@@ -1,7 +1,11 @@
-var Team = function(name, id, drivers){
+var Team = function(name, id, drivers, car){
     var name = name,
         drivers = drivers,
         id = id;
+        car = car;
+    this.driverFirst = function() {return drivers[0]};
+    this.driverSecond = function() {return drivers[1]};
 
-
+    this.car = function() {return car;};
+    this.id = function() {return id; };
 };
